0b305cdb2eefff9fee3fb4cabc3a6b1abc241b33
gregoire <gregoire@192.168.0.42>
2015-12-06 17:04:06 +0100

    màj main

--- M3103_git/src/tp99_Training/Ex1DS1_Robot.java
+++ M3103_git/src/tp99_Training/Ex1DS1_Robot.java
@@ -2,6 +2,10 @@ package tp99_Training;
 
 public class Ex1DS1_Robot {
 
+	public static void main(String args[]){
 	System.out.println("rien à dire");
+	System.out.println("rien à dire");
+	
+	}
 	
 }
